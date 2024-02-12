7969c93f304024f062eabd06b90ff218545f6596
Todd Hammersmith <thammersmith@gmail.com>
2024-02-11 22:56:15 -0600

    dataInputer wrapped in begin/end transaction.

--- dataInputer.php
+++ dataInputer.php
@@ -2,23 +2,19 @@
 
 //create if not exists or open sqlite3 database
 $db = new SQLite3('humap2.db');
+
 //create table if not exists, named pin
-$db->exec('CREATE TABLE IF NOT EXISTS pin (id INTEGER PRIMARY KEY, pin TEXT)');
-//create proteinA column if not exists
-$db->exec('ALTER TABLE pin ADD COLUMN proteinA TEXT');
-//create proteinB column if not exists
-$db->exec('ALTER TABLE pin ADD COLUMN proteinB TEXT');
-//create interaction column if not exists
-$db->exec('ALTER TABLE pin ADD COLUMN interaction TEXT');
+$db->exec('CREATE TABLE IF NOT EXISTS pin (id INTEGER PRIMARY KEY, proteinA TEXT, proteinB TEXT, interaction TEXT)');
 
 //load humap2_ppis_genename_20200821.pairsWprob.csv
 $pisCsv = fopen('humap2_ppis_genename_20200821.csv', 'r');
-//$pisCsv = fopen('humap2_ppis_genename_20200821_dummy.csv', 'r');
+
+//start transaction
+$db->exec('BEGIN');
 
 //iterate through each row of the csv file add to the database
 while (($data = fgetcsv($pisCsv, 0, ",")) !== FALSE) {
   $stmt = $db->prepare('INSERT INTO pin (proteinA, proteinB, interaction) VALUES (:proteinA, :proteinB, :interaction)');
-  //$stmt->bindValue(':pin', $data[0], SQLITE3_TEXT);
   $stmt->bindValue(':proteinA', $data[0], SQLITE3_TEXT);
   $stmt->bindValue(':proteinB', $data[1], SQLITE3_TEXT);
   $stmt->bindValue(':interaction', $data[2], SQLITE3_TEXT);
@@ -28,6 +24,9 @@ while (($data = fgetcsv($pisCsv, 0, ",")) !== FALSE) {
   }
 }
 
+//commit transaction
+$db->exec('COMMIT');
+
 // Close the CSV file
 fclose($pisCsv);
 
@@ -36,11 +35,15 @@ $db->close();
 
 //open the database
 $db = new SQLite3('humap2.db');
+
 //create table if not exist HuMAP2_ID,Confidence,Uniprot_ACCs,genenames
 $db->exec('CREATE TABLE IF NOT EXISTS HuMAP2_ID (id INTEGER PRIMARY KEY, HuMAP2_ID TEXT, Confidence TEXT, Uniprot_ACCs TEXT, genenames TEXT)');
 
 //load humap2_complexes_20200809.csv
 $dataCsv2 = fopen('humap2_complexes_20200809.csv', 'r');
+
+//start transaction
+$db->exec('BEGIN');
 
 //iterate through each row of the csv file add to the database
 while (($data = fgetcsv($dataCsv2, 0, ",")) !== FALSE) {
@@ -55,6 +58,10 @@ while (($data = fgetcsv($dataCsv2, 0, ",")) !== FALSE) {
   }
 }
 
+//commit transaction
+$db->exec('COMMIT');
+
 //close the database
 $db->close();
+
 ?>
